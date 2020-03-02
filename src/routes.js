f719f1689ef77c452b7061e3d0894121227d4a5c
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-03-02 00:23:22 -0300

    react router and statusBar

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#7159c1',
+          },
+          headerTintColor: '#fff',
+          headerBackTitleVisible: false,
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={Main}
+          options={{title: 'Usuários'}}
+        />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
