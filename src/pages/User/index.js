cb994994deeb2b808c446a87caa76fbb551e1c02
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-03-09 01:37:54 -0300

    chamada a API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
+import Proptypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User(props) {
-  console.tron.log(props);
-  const {
-    params: {user},
-  } = props.route;
+export default class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: [],
+    };
+  }
 
-  return <View />;
+  async componentDidMount() {
+    const {
+      route: {
+        params: {user},
+      },
+    } = this.props;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {stars} = this.state;
+    return <View />;
+  }
 }
+
+User.propTypes = {
+  route: Proptypes.shape({
+    params: Proptypes.shape({
+      user: Proptypes.shape({
+        login: Proptypes.string,
+      }),
+    }),
+  }).isRequired,
+};
